6dec5d18498521bfcb08d5fd08660c40442053fa
Ruben-1058461 <1058461@hr.nl>
2024-09-25 15:21:36 +0200

    First Commit

---
+++ officecalender/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+
+// Add localhost and port
+app.Urls.Add("http://localhost:5002");
+
+// Test hello
+app.MapGet("" , ()=> "Hello");
+
+
+
+
+
+app.Run();
+
