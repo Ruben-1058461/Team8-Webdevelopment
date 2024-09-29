19ad14ae67473a87a161533ff7690027512a40ee
Luis Manuel <1060597@hr.nl>
2024-09-29 15:51:00 +0200

    Werkt met mijn locale database(MYSQL), laat alleen ruwe data zien.

--- Dashboard/obj/Debug/net8.0/Dashboard.AssemblyInfo.cs
+++ Dashboard/obj/Debug/net8.0/Dashboard.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+128aa019023f5bb5989cc9dd2515d25cedaa9222")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7f235c0a830e1199f021c57785081f312006a2")]
+>>>>>>> 1d30aae (Werkt met mijn locale database(MYSQL), laat alleen ruwe data zien.)
 [assembly: System.Reflection.AssemblyProductAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
