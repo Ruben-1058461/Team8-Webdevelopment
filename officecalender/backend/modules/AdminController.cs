dd6eca08bf179cb85e6f17f146f8e255fcd0761d
Thijs-1051036 <1051036@hr.nl>
2024-09-30 16:04:39 +0200

    Added: GET for specific ID

--- officecalender/backend/modules/AdminController.cs
+++ officecalender/backend/modules/AdminController.cs
@@ -6,7 +6,8 @@ public class AdminController : ControllerBase
 {
     private static List<Admin> Admins = new List<Admin>
     {
-        new Admin { user_name = "Thijs", password = "Admin1", email = "Test@mail.com" }
+        new Admin { id = 1, user_name = "Thijs", password = "Admin1", email = "Test@mail.com" },
+        new Admin { id = 2, user_name = "Jeroen", password = "Admin2", email = "Test2@mail.com"}
     };
 
     // GET: api/Admin
@@ -15,5 +16,19 @@ public class AdminController : ControllerBase
     {
         return Ok(Admins);
     }
+
+    // GET: api/Admin/1
+    [HttpGet("{id}")]
+    public ActionResult<Admin> GetAdmin(int id)
+    {
+        // Corrected to use Admins instead of _admin
+        var admin = Admins.Find(a => a.user_name == Admins[id - 1].user_name);
+        if (admin == null)
+        {
+            return NotFound();
+        }
+        return Ok(admin);
+    }
+
 
 }
