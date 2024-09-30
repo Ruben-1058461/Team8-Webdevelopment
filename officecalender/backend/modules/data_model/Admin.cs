a0bd00495447bf4805e97ba8ef5a8ce0ce26593f
Thijs-1051036 <1051036@hr.nl>
2024-09-30 16:05:12 +0200

    Added: ID for Admin

--- officecalender/backend/modules/data_model/Admin.cs
+++ officecalender/backend/modules/data_model/Admin.cs
@@ -1,5 +1,6 @@
 public class Admin
 {
+    public required int id { get; set; }
     public required string user_name { get; set; }
     public required string password { get; set; }
     public required string email { get; set; }
