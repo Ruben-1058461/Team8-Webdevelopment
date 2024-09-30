128aa019023f5bb5989cc9dd2515d25cedaa9222
Luis Manuel <1060597@hr.nl>
2024-09-30 12:28:29 +0200

    Werkt nu

--- Dashboard/obj/Debug/net8.0/Dashboard.AssemblyInfo.cs
+++ Dashboard/obj/Debug/net8.0/Dashboard.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c7f235c0a830e1199f021c57785081f312006a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d30aae1c0e79c5624e97dc2f298bf5a8ef61000")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
