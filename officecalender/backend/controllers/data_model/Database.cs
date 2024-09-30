d72ba609b64bf9d978c19607306fd5bdde3f4e5b
Thijs-1051036 <1051036@hr.nl>
2024-09-30 17:35:06 +0200

    Added: CRUD for user

--- officecalender/backend/controllers/data_model/Database.cs
+++ officecalender/backend/controllers/data_model/Database.cs
@@ -8,9 +8,11 @@ public class Database : DbContext
     }
 
     public DbSet<Admin> Admins { get; set; }
+    public DbSet<User> Users { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Admin>().ToTable("admin");
+        modelBuilder.Entity<User>().ToTable("user");
     }
 }
