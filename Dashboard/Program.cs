3e90c1e391f82fbcf34a6d120ad94a962d40e79e
Luis Manuel <1060597@hr.nl>
2024-09-30 12:28:29 +0200

    Werkt nu

--- Dashboard/Program.cs
+++ Dashboard/Program.cs
@@ -1,66 +1,56 @@
-using MySqlConnector;  // Make sure you include this at the top
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Sqlite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-
-// Registering MySQL connection string as a singleton (optional, can be scoped or transient as needed)
-builder.Services.AddSingleton<MySqlConnectionStringBuilder>(sp => new MySqlConnectionStringBuilder
-{
-    Server = "localhost",
-    UserID = "root",
-    Password = "speedylight101",
-    Database = "mydb"
-});
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
 // Add a new endpoint to interact with the database
-app.MapGet("/users", async (MySqlConnectionStringBuilder connectionStringBuilder) =>
+app.MapGet("/users", async () =>
 {
-    // Open a connection asynchronously
-    using var connection = new MySqlConnection(connectionStringBuilder.ConnectionString);
-    await connection.OpenAsync();
-
-    // Create a DB command and set the SQL statement
-    using var command = connection.CreateCommand();
-    command.CommandText = @"SELECT * FROM Users;";
-
-    // Execute the command and read the results
-    using var reader = await command.ExecuteReaderAsync();
-    var users = new List<object>();
-
-    while (reader.Read())
+    // Open a connection with SQLite using the provided connection string
+    using (var connection = new SqliteConnection("Data Source=database.db"))
     {
-        var user = new
-        {
-            id = reader.GetInt32("user_id"),
-            name = reader.GetString("userName")
-        };
-        users.Add(user);
-    }
+        // Open the connection
+        await connection.OpenAsync();
 
-    return Results.Ok(users); // Return the list of users as JSON
+        // Create a new command and set the SQL query
+        using (var command = connection.CreateCommand())
+        {
+            command.CommandText = @"
+                SELECT id, user_name
+                FROM admin;
+            ";
+
+            // Execute the command and read the results
+            using (var reader = await command.ExecuteReaderAsync())
+            {
+                var users = new List<object>();
+
+                // Loop through the data returned by the query
+                while (await reader.ReadAsync())
+                {
+                    var user = new
+                    {
+                        id = reader.GetInt32(0), // Index-based retrieval
+                        name = reader.GetString(1)
+                    };
+                    users.Add(user);
+                }
+
+                // Return the list of users as a result
+                return Results.Ok(users);
+            }
+        }
+    }
 });
+
 
 app.Run();
