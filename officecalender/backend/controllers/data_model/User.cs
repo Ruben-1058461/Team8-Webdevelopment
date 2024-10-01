e3a03fed691d929d4f46f1a1b4771858c4dd5496
Ruben <ruben@gateway.nl>
2024-10-01 18:57:49 +0200

    Fix salt

--- officecalender/backend/controllers/data_model/User.cs
+++ officecalender/backend/controllers/data_model/User.cs
@@ -7,6 +7,7 @@ public class User
     public required string password { get; set; }
     public required string email { get; set; }
     public required string recurring_days { get; set; }
+    public string Salt { get; set; }
     public bool is_admin { get; set; } = false;
 
     // Add a property for the salt
