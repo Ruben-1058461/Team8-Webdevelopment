aec3507fcc18472232f72972c22ffff25ab50bbb
Ruben <ruben@gateway.nl>
2024-10-02 09:38:57 +0200

    Change Backend

--- officecalender/backend/controllers/AuthController.cs
+++ officecalender/backend/controllers/AuthController.cs
@@ -22,7 +22,8 @@ namespace officecalender.backend.Controllers
         public async Task<IActionResult> Login([FromBody] LoginRequest request)
         {
             // Find the user by first name
-            var user = await _context.Users.SingleOrDefaultAsync(u => u.first_name == request.FirstName);
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.email == request.Email);
+
 
             // Check if the user exists and if the password is correct
             if (user == null || !VerifyPassword(request.Password, user.password, user.salt))
@@ -61,7 +62,8 @@ namespace officecalender.backend.Controllers
     // Request model for login
     public class LoginRequest
     {
-        public string FirstName { get; set; }
+        public string Email { get; set; }
+
         public string Password { get; set; }
     }
 }
