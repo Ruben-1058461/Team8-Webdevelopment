c09532438189a3a877b87cdc1aaf1899b795aae1
Ruben-1058461 <1058461@hr.nl>
2024-10-04 16:31:26 +0200

    delete salt

--- officecalender/backend/controllers/data_model/User.cs
+++ officecalender/backend/controllers/data_model/User.cs
@@ -7,9 +7,6 @@ public class User
     public required string password { get; set; }
     public required string email { get; set; }
     public required string recurring_days { get; set; }
-    public string Salt { get; set; }
     public bool is_admin { get; set; } = false;
 
-    // Add a property for the salt
-    public byte[] salt { get; set; }
 }
