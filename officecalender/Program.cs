0225092abc6cde750049c9153d14a6a391d8e1f9
Rushillachmon <125268172+Rushillachmon@users.noreply.github.com>
2024-10-04 15:15:37 +0200

    Update Program.cs

--- officecalender/Program.cs
+++ officecalender/Program.cs
@@ -32,45 +32,7 @@ DatabaseInitializer.InitializeDatabase();
 // Define routes
 app.MapGet("", () => "Hello");
 
-// Add a new endpoint to interact with the database
-app.MapGet("/adminlist", async () =>
-{
-    using (var connection = new SqliteConnection("Data Source=database.db"))
-    {
-        await connection.OpenAsync();
 
-        using (var command = connection.CreateCommand())
-        {
-            command.CommandText = @"
-                SELECT id, user_name, email, is_admin
-                FROM admin;
-            ";
-
-            using (var reader = await command.ExecuteReaderAsync())
-            {
-                var users = new List<object>();
-                while (await reader.ReadAsync())
-                {
-                    var user = new
-                    {
-                        id = reader.GetInt32(0),
-                        name = reader.GetString(1),
-                        email = reader.GetString(2),
-                        is_admin = reader.GetString(3)
-                    };
-                    users.Add(user);
-                }
-
-                return Results.Ok(users);
-            }
-        }
-    }
-});
-
-// Use CORS and routing
-app.UseCors("AllowReactApp");
-app.UseRouting();
-app.MapControllers();
 
 // Start the application
 app.Run();
