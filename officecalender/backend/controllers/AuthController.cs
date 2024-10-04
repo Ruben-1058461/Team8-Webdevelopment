82cd4c2dd00d61979634680d5f9547b856183590
Ruben-1058461 <1058461@hr.nl>
2024-10-04 16:48:29 +0200

    Add jwtsecuritytokenhandler

--- officecalender/backend/controllers/AuthController.cs
+++ officecalender/backend/controllers/AuthController.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.IdentityModel.Tokens.Jwt; // JwtSecurityTokenHandler
 using officecalender.backend.Models; // Import the Models namespace
 
 namespace officecalender.backend.Controllers
