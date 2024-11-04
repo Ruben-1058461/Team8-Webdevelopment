d00c619f1a460641506bc9815a47c6484d363007
Luis Manuel <1060597@hr.nl>
2024-11-04 21:23:05 +0100

    Comments in the rest file

--- officecalender/backend/controllers/SignUpEventController.cs
+++ officecalender/backend/controllers/SignUpEventController.cs
@@ -36,19 +36,6 @@ public class SignUpEventController : ControllerBase
     }
    
 
-     [HttpPost("exampleUserId")]
-public async Task<IActionResult> exampleIdAsync([FromBody] EventAttendanceRequest request)
-{
-    int? userId = HttpContext.Session.GetInt32("UserId");
-    if (!userId.HasValue)
-    {
-        return Unauthorized("User ID not found in session.");
-    }
-    
-    return Ok($"User ID from session: {userId.Value}");
-    
-}
-
  [HttpPost("SignUpEventUser")] //Post Adds user to the event
 public async Task<IActionResult> addUserToEvent([FromBody] EventAttendanceRequest request)
 {
